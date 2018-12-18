65be9213482026769c2b3a7cde97b733e9e0b052
alecbert <alec.bertossa@gmail.com>
2018-12-17 23:10:21 -0500

    The race condition is still there but writing to the console slows it down enough that it doesn't happen. I'll figure out how to do file printing right again tomorrow.

--- CreateTracker.py
+++ CreateTracker.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import io 
 from datetime import datetime
 
 #We could also check that the year isn't 18 but whatever
@@ -20,33 +21,30 @@ def get_everything(username = 'erg'):
         #chunks come in 300s
         offset+=CHUNKSIZE
         
-    print(len(everything))
     return everything 
 
 jsonItems = get_everything()
-print(len(jsonItems))
 #aggregator
 epsLeft = 0
 errorTitle = ''
 
-#printing all this out to a text file
-try:
-    with open('animeTracker.txt', 'w') as fileWriter:
-        for x in jsonItems:
-            errorTitle = x['anime_title']
-            if datetime.strptime(x['start_date_string'] or "01-01-18", '%m-%d-%y') < START_2018:
-                #This is supposed to default to Jan 1 2018 for the finish date if one isn't in there (this default date shouldn't trip making a DONE! row)
-                if datetime.strptime(x['finish_date_string'] or "01-01-18", "%m-%d-%y") > START_2018:
-                    fileWriter.write(str(x['anime_title']))
-                    fileWriter.write(' #' + str(x['anime_id']) + ' DONE!\n')
-                #Only print out what's still being watched in that case
-                elif x['status'] == 1:
-                    fileWriter.write(str(x['anime_title']))
-                    fileWriter.write(' #' + str(x['anime_id']) + ' ' + str(x['num_watched_episodes']) + '/' + str(x['anime_num_episodes']) + '\n')
-                    epsLeft += (x['anime_num_episodes'] - x['num_watched_episodes'])
+file_str = io.StringIO()
+for x in jsonItems:
+    errorTitle = x['anime_title']
+    if datetime.strptime(x['start_date_string'] or "01-01-18", '%m-%d-%y') < START_2018:
+        #This is supposed to default to Jan 1 2018 for the finish date if one isn't in there (this default date shouldn't trip making a DONE! row)
+        if datetime.strptime(x['finish_date_string'] or "01-01-18", "%m-%d-%y") > START_2018:
+            file_str.write(str(x['anime_title']))
+            file_str.write(' #' + str(x['anime_id']) + ' DONE!\n')
+        #Only print out what's still being watched in that case
+        elif x['status'] == 1:
+            file_str.write(str(x['anime_title']))
+            file_str.write(' #' + str(x['anime_id']) + ' ' + str(x['num_watched_episodes']) + '/' + str(x['anime_num_episodes']) + '\n')
+            epsLeft += (x['anime_num_episodes'] - x['num_watched_episodes'])
 
-        fileWriter.write('\nEpisodes left ' + str(epsLeft))
-except Exception as e:
-    print('Something went wrong printing: ')
-    print(e)
-    print(errorTitle)
+file_str.write('\nEpisodes left ' + str(epsLeft))
+
+#printing all this out to a text file
+print(file_str.getvalue())
+
+file_str.close()
